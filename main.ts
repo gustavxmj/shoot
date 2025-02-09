7f16114a9bbdec5bc445febb6589154920b7942b
Gustav X <sillygorilla42@gmail.com>
2025-02-09 07:24:11 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let smiley = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . 5 5 5 5 5 5 . . . . . . . . 
+    . 5 5 5 5 5 5 5 5 . . . . . . . 
+    5 5 5 5 5 9 9 5 5 5 . . . . . . 
+    5 5 5 5 5 9 f 5 5 5 . 5 5 . . . 
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    5 5 5 5 5 f f f f f 5 5 5 5 . . 
+    5 5 5 e e e e e e e e 5 5 e . . 
+    5 5 5 e e e e e e e e e e f e . 
+    . 5 5 e e e e e e e e e e f e . 
+    . . 5 e e e e e e e e e e f e . 
+    . . . e e e e e e e e e e e . . 
+    . . 5 e 5 2 2 e . . . . . . . . 
+    . . 5 e 5 5 2 e . . . . . . . . 
+    . . . 5 e e e . . . . . . . . . 
+    `, SpriteKind.Player)
